dfe14bc95b25206770dcd842a55a1099ceab4e85
Bernard Thompson <bethomps@redhat.com>
2019-07-01 19:41:26 +0000

    CntactPublic component implemented

--- stories/index.js
+++ stories/index.js
@@ -7,8 +7,9 @@ import Contact from '../src/components/contact/';
 import ContactList from '../src/components/contactList/';
 import { action } from '@storybook/addon-actions';
 import { MemoryRouter, Route } from "react-router";
-import PublicProfile from '../src/components/contactPublic/publicProfile'
-import ContactPrivateDetails from '../src/components/contactPrivate/'
+import PublicProfile from '../src/components/contactPublic/publicProfile';
+import ContactPrivateDetails from '../src/components/contactPrivate/';
+import ContactPublicDetails from '../src/components/contactPublic/';
 
 const sample = {
   name: {first:'Joe', last:'Bloggs'},
@@ -83,3 +84,11 @@ storiesOf("Contact List App/Contact Page/ContactPrivate", module)
 .add("default", () => ( 
    <ContactPrivateDetails user={sample}/>
 ));
+
+storiesOf("Contact List App/Contact Page/Public", module)
+.addDecorator(story => (
+  <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+))
+.add("default", () => { 
+   return <ContactPublicDetails user={sample}/>
+});
